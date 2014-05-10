046a4b49d65957f84e9b30bd1b46dbc0000bae55
7flying <diez.f.irene@gmail.com>
2014-05-11 01:11:06 +0200

    simple Communicator + domain object

---
+++ greenhouse/server_greenhouse/src/com/sevenflying/server/domain/exceptions/GreenhouseServerException.java
@@ -0,0 +1,7 @@
+package com.sevenflying.server.domain.exceptions;
+
+public class GreenhouseServerException extends Exception{
+	
+	private static final long serialVersionUID = -5610381313148967700L;
+
+}
