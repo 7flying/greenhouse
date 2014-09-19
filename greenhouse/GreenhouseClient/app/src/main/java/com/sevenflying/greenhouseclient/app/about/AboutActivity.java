782c9fb0d4e6f945f1f856e45bafe9e4b01c76d8
7flying <diez.f.irene@gmail.com>
2014-09-19 19:24:05 +0200

    Settings & About skeletons

---
+++ greenhouse/GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/app/about/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.sevenflying.greenhouseclient.app.about;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/** Shows info about the app
+ * Created by 7flying on 18/09/2014.
+ */
+public class AboutActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ greenhouse/GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/app/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.sevenflying.greenhouseclient.app.settings;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/** This activity is used to provide advanced options.
+ * Created by 7flying on 18/09/2014.
+ */
+public class SettingsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
