3e7519f905d4d4a70e1db3cd23ac7d234740c748
7flying <diez.f.irene@gmail.com>
2015-02-13 23:28:25 +0100

    Actuator modification and removal tasks

--- GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/net/ActuatorModificationTask.java
+++ GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/net/ActuatorModificationTask.java
@@ -1,19 +1,63 @@
 package com.sevenflying.greenhouseclient.net;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /** Requests an actuator modification using a background task.
  * Created by flying on 10/02/15.
  */
 public class ActuatorModificationTask extends AsyncTask<String, Void, String> {
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
+    private Communicator comm;
+    private String host;
+    private int serverPort;
+
+    public ActuatorModificationTask(Context context) {
+        this.comm = new Communicator(context);
     }
 
     @Override
+    protected void onPreExecute() {
+        this.serverPort = comm.getServerPort();
+        this.host = comm.getServer();
+    }
+    @Override
     protected String doInBackground(String... params) {
-        return null;
+        // 0- name (encoded)
+        // 1- id
+        // 2- type
+        // 3- (optional) sensor id
+        // 4- (optional) compare type
+        // 5- (optional) compare value
+        String ret = null;
+        if (params.length == 3 || params.length == 6) {
+            try {
+                InetAddress add = InetAddress.getByName(host);
+                Socket s = new Socket(add, serverPort);
+                ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+                ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+                oos.writeObject(Commands.UPDATE_ACTUATOR);
+                oos.flush();
+                String send = params[0] + ":" + params[1] + ":" + params[2];
+                if (params.length == 6)
+                    send += params[3] + ":" + params[4] + ":" + params[5];
+                oos.writeObject(send);
+                oos.flush();
+                String response = (String) ois.readObject();
+                ret = response.equals(Constants.OK) ? Constants.OK : response;
+                s.close();
+                oos.close();
+                ois.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return ret;
     }
 }
--- GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/net/ActuatorRemovalTask.java
+++ GreenhouseClient/app/src/main/java/com/sevenflying/greenhouseclient/net/ActuatorRemovalTask.java
@@ -1,19 +1,56 @@
 package com.sevenflying.greenhouseclient.net;
 
+import android.content.Context;
 import android.os.AsyncTask;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /** Requests an actuator removal using a background task.
  * Created by flying on 10/02/15.
  */
 public class ActuatorRemovalTask extends AsyncTask<String, Void, String> {
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
+    private Communicator comm;
+    private String host;
+    private int serverPort;
+
+    public ActuatorRemovalTask(Context context) {
+        this.comm = new Communicator(context);
     }
 
     @Override
-    protected String doInBackground(String... params) {
-        return null;
+    protected void onPreExecute() {
+        this.serverPort = comm.getServerPort();
+        this.host = comm.getServer();
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+        // needs the pinid
+        if (strings.length != 1)
+            return null;
+        String ret = null;
+        try {
+            InetAddress add = InetAddress.getByName(host);
+            Socket s = new Socket(add, serverPort);
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+            oos.writeObject(Commands.DELETE_ACTUATOR);
+            oos.flush();
+            oos.writeObject(strings[0]);
+            oos.flush();
+
+            String response = (String) ois.readObject();
+            ret = response.equals(Constants.OK) ? Constants.OK : response;
+
+            s.close();
+            oos.close();
+            ois.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 }
